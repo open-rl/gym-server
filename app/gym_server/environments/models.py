a265e193f6304e0950576d6fa68a2058bbd82be7
denizs <deniz.saner@rwth-aachen.de>
2018-04-29 15:04:51 +0200

    Scaffolded `environments` app and added it to `INSTALLED_APPS`

---
+++ app/gym_server/environments/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class Environment(models.Model):
+    '''
+    The `Environment` model represents an individual reinforcement learning task.
+    '''
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+    # may be obsolete
+    high_score = models.FloatField()
+    video_url = models.URLField()
