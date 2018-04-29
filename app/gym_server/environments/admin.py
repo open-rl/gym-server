8a70949868099bb3f6ff46527dc334c25d9ac283
denizs <deniz.saner@rwth-aachen.de>
2018-04-29 15:23:33 +0200

    Added admin support for `Environment`

--- app/gym_server/environments/admin.py
+++ app/gym_server/environments/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from environments.models import Environment
+
 
 # Register your models here.
+class EnvironmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'video_url')
+    fields = ('name', 'description', 'video_url')
+
+
+admin.site.register(Environment, EnvironmentAdmin)
