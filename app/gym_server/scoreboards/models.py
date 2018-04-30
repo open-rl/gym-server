e88ea0c458b4c63e955201648a59a9ba77489b25
denizs <deniz.saner@rwth-aachen.de>
2018-04-30 16:37:41 +0200

    Initial draft of `EvaluationRun` and corresponding `ModelAdmin` + migrations

--- app/gym_server/scoreboards/models.py
+++ app/gym_server/scoreboards/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from environments.models import Environment
+from allauth.socialaccount.models import SocialAccount
 
-# Create your models here.
+
+class EvaluationRun(models.Model):
+    user = models.ForeignKey(SocialAccount,
+                             on_delete=models.CASCADE)
+    env = models.ForeignKey(Environment, related_name='eval_runs',
+                            on_delete=models.CASCADE)
+    s3_path = models.CharField(max_length=255)
+    high_score = models.FloatField()
+    # what else?
+
+    class Meta:
+        verbose_name = 'Evaluation Run'
