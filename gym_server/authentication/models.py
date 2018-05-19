fb5639cc6212b87bc79116190b4e55503ff25ded
denizs <deniz.saner@rwth-aachen.de>
2018-05-19 17:47:57 +0200

    Implemented `AuthToken` model and migrations.

--- gym_server/authentication/models.py
+++ gym_server/authentication/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class AuthToken(models.Model):
+    '''
+    The `AuthToken` model represents a simple token, which gets automatically
+    generated when a new user signs up. It is then used to authenticate this
+    user, whenever he or she uploads an evaluation to our S3 bucket.
+
+    The reason not to use the users `SocialToken` from allauth is that this may
+    be used to extract all personal information from github.
+    '''
+    account = models.OneToOneField(User, related_name='openrl_token',
+                                   on_delete=models.CASCADE)
+    token = models.CharField(max_length=255, blank=False, null=False,
+                             default='test-token')
